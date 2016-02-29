e2f3c565b13127c10643ed9e737c1c2951891580
js571 <1775769070@qq.com>
2016-02-29 12:06:29 +0800

    modify

--- index.js
+++ index.js
@@ -16,48 +16,48 @@ module.exports = function (options) {
         var str = contents.toString('utf8');
         var regex = new RegExp('\([?]*[0-9]+?)('+ options.unit +')','gi');
         // var regex2 = new RegExp('([a-zA-Z|-]*)[\\s]*\\:[\\s]*([0-9]*)[\\s]*px[\\s]*[;]*','gi');
-        var regex2 = new RegExp('([a-zA-Z|-]*)[\\s]*\\:[\\s]*([0-9]*)[\\s]*px[\\s]*[;]*','gi');
+        var regex2 = new RegExp('font\\-size[\\s]*\\:[\\s]*([0-9]*)[\\s]*px[\\s]*[;]*','gi');
         str = str.replace(regex,function($0,$1) {
             var res  = $1*320/options.width/20;
             return res + 'rem';
         });
 
 
-        str = str.replace(regex2,function($0,$1,$2) {
-            var tmp = `${$1}: ${$2/2}px;
+        str = str.replace(regex2,function($0,$2) {
+            var tmp = `font-size: ${$2/2}px;
                         [data-dpr="1.5"] &{
-                            ${$1}: ${$2*1.5/2}px;
+                            font-size: ${$2*1.5/2}px;
                         }
                         [data-dpr="2"] &{
-                            ${$1}: ${$2}px;
+                            font-size: ${$2}px;
                         }
                         [data-dpr="2.5"] &{
-                            ${$1}: ${$2*2.5/2}px;
+                            font-size: ${$2*2.5/2}px;
                         }
                         [data-dpr="2.75"] &{
-                            ${$1}: ${$2*2.75/2}px;
+                            font-size: ${$2*2.75/2}px;
                         }
                         [data-dpr="3"] &{
-                            ${$1}: ${$2*3/2}px;
+                            font-size: ${$2*3/2}px;
                         }
                         [data-dpr="3.25"] &{
-                            ${$1}: ${$2*3.25/2}px;
+                            font-size: ${$2*3.25/2}px;
                         }
                         [data-dpr="3.5"] &{
-                            ${$1}: ${$2*3.5/2}px;
+                            font-size: ${$2*3.5/2}px;
                         }
                         [data-dpr="3.75"] &{
-                            ${$1}: ${$2*3.75/2}px;
+                            font-size: ${$2*3.75/2}px;
                         }
                         [data-dpr="4"] &{
-                            ${$1}: ${$2*4/2}px;
+                            font-size: ${$2*4/2}px;
                         }`;
             return tmp;
         });
 
 
         return new Buffer(str);
-    }    
+    }
 
     return through2.obj(function(file, enc, cb) {
         if (file.isNull()) {
